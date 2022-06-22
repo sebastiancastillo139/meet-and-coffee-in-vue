cc689a3cf2976979dd74f8ad157d68fbfc27876e
Sebastián Castillo <sebastiancastillo1309@gmail.com>
2022-06-22 15:17:53 -0400

    Anadido dist para deply

---
+++ js/about.c8faf6d5.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmeetandcoffee_vue"]=self["webpackChunkmeetandcoffee_vue"]||[]).push([[443],{5830:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});var u=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],s=n(1001),c={},l=(0,s.Z)(c,u,a,!1,null,null,null),r=l.exports}}]);
+//# sourceMappingURL=about.c8faf6d5.js.map
